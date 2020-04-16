b9ee7d3a210b1285d1d94136580f96ed7e83dfa5
judyz04 <di.zhou22@ycdsbk12.ca>
2020-04-16 13:44:36 -0400

    YESSS this is a Vibe

--- monthtoday.py
+++ monthtoday.py
@@ -1,9 +1,9 @@
-#import time
 month = str(input("Enter the month name: "))
-
-#if month == Sept or Apr or June or Nov:
-    #print("There are 30 days.")
-#elif month == Feb: 
-    #print("There are 28 or 29 days.")
-#else:
-    #print("There are 31 days.")
+d30 = {"April", "June", "September", "November"} 
+d32 = {"Feburary"}
+if month in d30:
+    print("There are 30 days.")
+elif month in d32:
+    print("There are 28 or 29 days.")
+else:
+    print("There are 31 days.")
