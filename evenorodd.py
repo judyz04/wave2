f5ede1d707dc615d88febdd00eef0b6e369f8097
judyz04 <di.zhou22@ycdsbk12.ca>
2020-04-16 13:26:30 -0400

    python sucks im going to cry

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+#get input
+n = int(input("Enter the number: "))
+
+if (n % 2) == 0:
+    print("The number is even.")
+else: 
+    print("The number is odd.")
---
+++ monthtoday.py
@@ -0,0 +1,9 @@
+#import time
+month = str(input("Enter the month name: "))
+
+#if month == Sept or Apr or June or Nov:
+    #print("There are 30 days.")
+#elif month == Feb: 
+    #print("There are 28 or 29 days.")
+#else:
+    #print("There are 31 days.")
